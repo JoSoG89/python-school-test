1393bd325384b58263c7af12925839a743a510ad
Aurelio Osorio <josorio@pcmloscabos.com>
2023-07-09 20:15:42 -0700

    new file:   inicial.py

--- inicial.py
+++ inicial.py
@@ -1,6 +1,13 @@
+<<<<<<< HEAD
 numero = 18
 edad = 15
 type (5)
 if edad < numero: print ("Menor") 
 else: print ("Mayor")
 print (len (""))
+=======
+numero = 20
+edad = 33
+type (5)
+
+>>>>>>> 8d36476 (	new file:   inicial.py)
