93b820f1c3ba71b35fcccb65debe9eb8488ae6d2
Aurelio Osorio <josorio@pcmloscabos.com>
2023-07-09 20:30:45 -0700

    modified:   inicial.py

--- inicial.py
+++ inicial.py
@@ -3,8 +3,3 @@ edad = 15
 type (5)
 if edad < numero: print ("Menor") 
 else: print ("Mayor")
-print (len (""))
-=======
-numero = 20
-edad = 33
-type (5)
--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-test
+# python-school-test
